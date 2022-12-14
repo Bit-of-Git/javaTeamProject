7bd708cd3078a5a3c3e3057791f75077e1ab6edd
orangebun <98849833+orangebun@users.noreply.github.com>
2022-12-14 16:50:17 -0500

    Update ConnectionCheck.java

--- src/Database/ConnectionCheck.java
+++ src/Database/ConnectionCheck.java
@@ -1,8 +1,9 @@
 package Database;
+
 import java.sql.*;
 public class ConnectionCheck {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 		 String conURL = "jdbc:sqlserver://idiashroud.database.windows.net:1433;database=Project;user=pleasework@idiashroud;password=GSUCIS3270!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 		 String user = "pleasework@idiashroud";
 		 String pass = "GSUCIS3270!";
@@ -12,5 +13,5 @@ public class ConnectionCheck {
 		 } catch (Exception e) {
 		  e.printStackTrace();
 		 }
-	} 
+	}
 }
