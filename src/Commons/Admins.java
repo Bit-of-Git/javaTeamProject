78e657bdb27491c0a02af2a3a090bc51b31a7a7c
orangebun <98849833+orangebun@users.noreply.github.com>
2022-12-30 18:12:51 -0500

    Update Admins.java

--- src/Commons/Admins.java
+++ src/Commons/Admins.java
@@ -1,12 +1,12 @@
 package Commons;
 
-public class Admins extends Customer implements Users {
+public class Admins extends Customer{
 	public Admins(String firstName, String lastName, String username, String password, String address, String state, 
-			int zip, String country, String email, String securityQ, String securityA, int SSN) {
+			int zip, String country, String email, String securityQ1, String securityA1, String securityQ2, String securityA2, int SSN) {
 		//constructors
 		
 		super(firstName, lastName, username, password, address, 
-				state, zip, country, email, securityQ, securityA, SSN);
+				state, zip, country, email, securityQ1, securityA1, securityQ2, securityA2, SSN);
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.username = username;
@@ -16,8 +16,10 @@ public class Admins extends Customer implements Users {
 		this.zip = zip;
 		this.country = country;
 		this.email = email;
-		this.securityQ = securityQ;
-		this.securityA = securityA;
+		this.securityQ = securityQ1;
+		this.securityA = securityA1;
+		this.securityQ = securityQ2;
+		this.securityA = securityA2;
 		this.ssn = SSN;
 		}
 }
